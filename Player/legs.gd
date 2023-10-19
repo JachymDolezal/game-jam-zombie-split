ea247527b12969dee2a9fa64132416de4fe7f0eb
JachymDolezal <jachym.dolezal@gmail.com>
2023-10-19 18:11:16 +0200

    add torso and legs

---
+++ Player/legs.gd
@@ -0,0 +1,61 @@
+extends CharacterBody2D
+
+
+const SPEED = Game.Legs_speed
+const JUMP_VELOCITY = Game.Legs_jump_speed
+var can_transform_back = false
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func transform_to_normal():
+	# get torso and remove it
+	var torso = get_parent().get_node("Torso")
+	
+	# instanciate normal body
+	var normal_body = preload("res://Player/zombie.tscn").instantiate()
+	get_parent().add_child(normal_body)
+	# set position
+	normal_body.position = self.position
+	torso.queue_free()
+	# remove self
+	self.queue_free()
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("Up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	if (Input.is_action_just_pressed("Down")) and can_transform_back:
+		transform_to_normal()
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("Left", "Right")
+	if direction:
+		# flip Sprite2D is the direction is to left
+		if direction < 0:
+			$Sprite2D.flip_h = true
+		else:
+			$Sprite2D.flip_h = false
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	print(body)
+	if body.name == 'Torso':
+		can_transform_back = true
+		print("torso in")
+
+
+func _on_area_2d_body_exited(body):
+	if body.name == 'Torso':
+		can_transform_back = false
+		print('torso out')
