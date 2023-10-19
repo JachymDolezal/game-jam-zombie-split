2b986e58b24fe73b2a123d641729410a15a77e92
JachymDolezal <jachym.dolezal@gmail.com>
2023-10-19 18:11:42 +0200

    add script with global variables

---
+++ Game.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+const Zombie_speed = 90.0
+const Zombie_jump_speed = -250
+const Zombie_weight = 3
+
+const Legs_speed = 140
+const Legs_jump_speed = -290
+const Legs_weight = 1
+
+const Torso_speed = 80
+const Torso_jump_speed = -190
+const Torso_weight = 2
