3fdcea7d2aeedf8e3ce841708759d8b30b4490e0
JachymDolezal <jachym.dolezal@gmail.com>
2023-10-21 22:52:16 +0200

    add sound for pickup

--- Meat.gd
+++ Meat.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var animation = $AnimatedSprite2D
+@onready var sounds = $AudioStreamPlayer2D
+@onready var animationplayer = $AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation.play("Idle")
@@ -8,10 +10,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#sounds.play()
 	pass
 
 
-
 func _on_area_2d_body_entered(body):
-	Game.points = Game.points + 1
-	self.queue_free()
+	if body.name == "Torso" or body.name == "Legs" or body.name == "Zombie":
+		Game.points = Game.points + 1
+		animationplayer.play("new_animation")
