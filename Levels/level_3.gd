fb2c0dd2e269f7ebde3ec01dd58dc50f2393a7f3
JachymDolezal <jachym.dolezal@gmail.com>
2023-10-21 23:28:43 +0200

    hotfit

--- Levels/level_3.gd
+++ Levels/level_3.gd
@@ -11,4 +11,4 @@ func _ready():
 func _process(delta):
 	#print(Game.points)
 	if Game.points == Game.required_points:
-		get_tree().change_scene_to_file("res://level_selection.tscn")
+		get_tree().change_scene_to_file("res://Ui/level_selection.tscn")
--- Levels/level_4.gd
+++ Levels/level_4.gd
@@ -11,4 +11,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Game.points == Game.required_points:
-		get_tree().change_scene_to_file("res://level_selection.tscn")
+		get_tree().change_scene_to_file("res://Ui/level_selection.tscn")
